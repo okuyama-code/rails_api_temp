6877ee78b68f39035fc66feacfb6e03c9a7f8042
okuyama-code <ooyy0121@gmail.com>
2024-06-30 20:28:21 +0900

    corsを3001に修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #許可したいURLを設定
-    origins 'http://localhost:8000'
+    origins 'http://localhost:3001'
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
