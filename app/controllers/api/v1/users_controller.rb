ce70b84505e616c5d3ae4e2ceed8dec09b176aa7
okuyama-code <ooyy0121@gmail.com>
2024-06-30 19:02:17 +0900

    usersとcurrentUserの取得のAPIを作成

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,43 @@
 module Api
   module V1
     class Api::V1::UsersController < ApplicationController
-      def create
-        # 条件に該当するデータがあればそれを返す。なければ新規作成
-        user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
+      def index
+        users = User.all
+        render json: { users: users }, status: :ok
+      end
+
+      def show
+        email = params[:id] == 'current' ? params[:email] : User.find(params[:id]).email
+
+        user = User.find_by(email: email)
+
         if user
-          head :ok
+          render json: { user: user }, status: :ok
         else
-          render json: { error: "ログインに失敗しました" }, status: :unprocessable_entity
+          render json: { error: "ユーザーが見つかりません" }, status: :not_found
         end
       rescue StandardError => e
         render json: { error: e.message }, status: :internal_server_error
       end
+
+      def create
+        user = User.find_by(email: params[:email])
+
+        if user
+          user.update(provider: params[:provider], uid: params[:uid])
+        else
+          user = User.create(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
+        end
+
+        if user.persisted?
+          render json: { user: user }, status: :ok
+        else
+          render json: { error: "ログインに失敗しました", details: user.errors.full_messages}, status: :unprocessable_entity
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: :internal_server_error
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   post 'api/v1/auth/:provider/callback', to: 'api/v1/users#create'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+    end
+  end
 end
