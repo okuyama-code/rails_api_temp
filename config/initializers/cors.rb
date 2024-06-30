804206f9f4d3ecce7b7ab3e1c0f6ab4896fd7d4b
okuyama-code <ooyy0121@gmail.com>
2024-06-30 14:00:19 +0900

    usersテーブルとコントローラー作成

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,13 +4,12 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    #許可したいURLを設定
+    origins 'http://localhost:8000'
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
